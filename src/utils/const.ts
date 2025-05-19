69fc76b0df62501385ed56847876c1c53377e014
Yiling-04 <yi0.liu@outlook.com>
2025-05-19 21:31:14 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -50,7 +50,7 @@ const SHOW_ELEVATION_GAIN = true;
 const RICH_TITLE = true;
 
 // IF you outside China please make sure IS_CHINESE = false
-const IS_CHINESE = true;
+const IS_CHINESE = false;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `户外运动 ${yearLength} 年 ` + ( year === 'Total' ? '' : `，地图展示的是 ${year} 年的轨迹`);
